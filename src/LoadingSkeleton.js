92469ec0384aa42b49a751cadd9840580eb882d3
murytarlah <murytarlah.dev@gmail.com>
2023-08-07 21:11:17 +0100

    :zap: added loader

--- src/LoadingSkeleton.js
+++ src/LoadingSkeleton.js
@@ -1,30 +1,10 @@
-const LoadingSkeleton = () => {     
-    return ( 
+const LoadingSkeleton = () => {
+    return (
         <div className="loader">
-            {
-                [...Array(250)].map((_,i) => (
-                    <div className="country">
-                        <div className="country">
-                            <div className="country-img">
-                                <div></div>
-                            </div>
-                            <div className="country-body">    
-                                <div className="country-title">
-                                    <h1></h1>
-                                </div>
-                                <div className="country-info">
-                                    <p><span></span></p>
-                                    <p><span></span></p>
-                                    <p><span></span></p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                ))
-            }
+            <div className="bar"></div>
         </div>
 
-     );
+    );
 }
- 
+
 export default LoadingSkeleton;     
